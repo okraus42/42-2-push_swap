d66d427f4efcf177ad882c5199a78e7dc1c9e770
Ondřej Kraus <okraus42+github@pm.me>
2023-03-10 16:58:05 +0100

    weird stuff happens

--- src_ps/ft_bruteforce.c
+++ src_ps/ft_bruteforce.c
@@ -6,7 +6,7 @@
 /*   By: okraus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:15:09 by okraus            #+#    #+#             */
-/*   Updated: 2023/03/10 15:10:39 by okraus           ###   ########.fr       */
+/*   Updated: 2023/03/10 16:45:34 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,65 @@ void	ft_ps_cpa(t_ps_info *inf, t_ps_info *it)
 	}
 }
 
+void	ft_ps_bfdo(t_ps_info *it, int i)
+{
+	ft_printf("i = %i\n", i);
+	while (ft_printf("%i\n", i % 11), i)
+	{
+		if (i % 11 == 0)
+			ft_ps_sa(&it->a_start, it);
+		else if (i % 11 == 1)
+			ft_ps_sb(&it->b_start, it);
+		else if (i % 11 == 2)
+			ft_ps_ss(&it->a_start, &it->b_start, it);
+		else if (i % 11 == 3)
+			ft_ps_pa(&it->a_start, &it->b_start, it);
+		else if (i % 11 == 4)
+			ft_ps_pb(&it->a_start, &it->b_start, it);
+		else if (i % 11 == 5)
+			ft_ps_ra(&it->a_start, it);
+		else if (i % 11 == 6)
+			ft_ps_rb(&it->b_start, it);
+		else if (i % 11 == 7)
+			ft_ps_rr(&it->a_start, &it->b_start, it);
+		else if (i % 11 == 8)
+			ft_ps_rra(&it->a_start, it);
+		else if (i % 11 == 9)
+			ft_ps_rrb(&it->b_start, it);
+		else if (i % 11 == 10)
+			ft_ps_rrr(&it->a_start, &it->b_start, it);
+		i /= 11;
+	}
+}
+
+int	ft_ps_bftest(t_ps_info *it, int i)
+{
+	t_dlist	*tmp;
+
+	ft_ps_bfdo(it, i);
+	ft_printf("it->b_size = %i\n", it->b_size);
+	if (!it->b_size)
+	{
+		tmp = it->a_start;
+		ft_printf("it->a_size = %i\n", it->a_size);
+		i = it->a_size;
+		while (i)
+		{
+			ft_printf("tmp = %p, i = %i, cont = %i\n", tmp, i, *(int *)tmp->content); 
+			tmp = tmp->prev;
+			ft_printf("%2Ctmp = %p, i = %i, cont = %i%0C\n", tmp, i, *(int *)tmp->content); 
+			if (*(int *)tmp->content == i)
+				i--;
+			else
+				return (0);
+		}
+		ft_printf("what?\n");
+		if (!i)
+			return (1);
+	}
+	return (0);
+}
+
 static int	ft_ps_bf2(t_ps_info *inf, t_ps_info *it)
 {
 	t_dlist **at;
@@ -90,18 +149,15 @@ static int	ft_ps_bf2(t_ps_info *inf, t_ps_info *it)
 		ft_ps_cpa(inf, it);
 		ft_ps_cpb(inf, it);
 		j = i;
+		/*
 		while (j)
 		{
 			ft_ps_pb(&it->a_start, &it->b_start, it);	
 			j--;
 		}
-		/*if (ft_ps_bftest(at, bt, it, i))
-		{
-			//free
-			return (i);
-		}*/
-		//ft_ps_bfreverse(at, bt, it, i);
-
+		*/
+		if (ft_ps_bftest(it, i))
+			j = -1;
 		ft_printf("\n\n%7CBRUTE FORCE YEAH %i%0C\n\n", i);
 		tmp = it->a_start;
 		j = 0;
@@ -129,7 +185,7 @@ static int	ft_ps_bf2(t_ps_info *inf, t_ps_info *it)
 		if (it->b_start)
 			ft_dlstclear2(&it->b_start);
 		i++;
-		if (i == 4)
+		if (j < 0)
 			return (i); 
 	}
 	return (-1);
@@ -147,9 +203,9 @@ int	ft_ps_bruteforce(t_ps_info *info)
 	ft_copyinfo(info, info_t);
 	i = ft_ps_bf2(info, info_t);
 	tmp = info_t->a_start;
-	i = 0;
 	tmp2 = info_t->b_start;
-	ft_printf("\n\n%7CBRUTE FORCE YEAH%0C\n\n");
+	ft_printf("\n\n%7CBRUTE FORCE == %i%0C\n\n", i);
+	i = 0;
 	if (info_t->a_size || info_t->b_size)
 	{
 		while (tmp && i < info_t->a_size)
