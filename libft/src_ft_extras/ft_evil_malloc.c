89c861f068b95a31b78c8bede22a69fb9ee1db9b
Ondřej Kraus <okraus42+github@pm.me>
2023-03-15 11:57:29 +0100

    fixed evil malloc

--- libft/src_ft_extras/ft_evil_malloc.c
+++ libft/src_ft_extras/ft_evil_malloc.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 10:20:43 by okraus            #+#    #+#             */
-/*   Updated: 2023/03/15 11:42:05 by okraus           ###   ########.fr       */
+/*   Updated: 2023/03/15 11:57:04 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	*ft_evil_malloc(size_t size)
 	char	*p;
 	size_t	i;
 
-	p = ft_malloc(size + 64);
+	p = malloc(size + 64);
 	if (p == NULL)
 		return (NULL);
 	i = 0;
-	while (i < nmemb * (size + 64))
+	while (i < (size + 64))
 	{
-		if (i < 32 || i > nmemb * (size + 32))
+		if (i < 32 || i > (size + 32))
 			p[i] = 0xFF;
 		else
 			p[i] = 1;
@@ -38,13 +38,13 @@ void	*ft_evil_malloc_plus(size_t size, size_t padding)
 	char	*p;
 	size_t	i;
 
-	p = ft_malloc(size + (2 * padding));
+	p = malloc(size + (2 * padding));
 	if (p == NULL)
 		return (NULL);
 	i = 0;
-	while (i < nmemb * (size + (2 * padding)))
+	while (i < (size + (2 * padding)))
 	{
-		if (i < padding || i > nmemb * (size + padding))
+		if (i < padding || i > size + padding)
 			p[i] = 0xFF;
 		else
 			p[i] = 1;
