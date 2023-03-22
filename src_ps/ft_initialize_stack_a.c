dd3380dd805aca2c01f4a1f5b69ec931b79dbb7a
Ondřej Kraus <okraus42+github@pm.me>
2023-03-22 17:19:29 +0100

    small fix

--- src_ps/ft_initialize_stack_a.c
+++ src_ps/ft_initialize_stack_a.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:54:29 by okraus            #+#    #+#             */
-/*   Updated: 2023/03/14 15:11:01 by okraus           ###   ########.fr       */
+/*   Updated: 2023/03/22 17:17:53 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,14 @@ void	ft_initialize_stack_a(int *arr, t_ps_info *info)
 	int		i;
 	t_dlist	*tmp;
 
-	i = 0;
-	while (i < info->a_size)
-		i++;
-	i = info->a_size;
-	info->b_size = 1;
-	i -= 1;
+	i = info->a_size - 1;
 	info->a_start = ft_dlstnew(&arr[i]);
 	tmp = info->a_start;
 	while (i)
 	{
-		tmp = ft_dlstnew(&arr[i - 1]);
-		ft_dlstadd(&info->a_start, tmp);
-		info->b_size += 1;
-		info->a_start = tmp;
 		i--;
+		tmp = ft_dlstnew(&arr[i]);
+		ft_dlstadd(&info->a_start, tmp);
+		info->a_start = tmp;
 	}
-	info->a_size = info->b_size;
-	info->b_size = 0;
 }
